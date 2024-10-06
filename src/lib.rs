8447fa1d4be74ef01c81729e41d383428e2c929d
Adel Amodwala <adel.amodwala@gmail.com>
2024-10-06 08:19:03 -0400

    Give up and use serde

--- src/lib.rs
+++ src/lib.rs
@@ -1,105 +1,35 @@
 use lazy_static::lazy_static;
 use regex::Regex;
+use serde_json::Value;
 
 lazy_static! {
     static ref ARRAY_REGEX: Regex = Regex::new(r"^\[(\d+)\]$").unwrap();
 }
 
-static JSON_SPACE: &str = " ";
-static JSON_COLON: &str = ":";
-static JSON_QUOTE: &str = "\"";
-static JSON_COMMA: &str = ",";
-static JSON_OPEN_BRACE: &str = "{";
-static JSON_CLOSE_BRACE: &str = "}";
-static JSON_OPEN_BRACKET: &str = "[";
-static JSON_CLOSE_BRACKET: &str = "]";
-
-
-pub fn search(haystack: String, search_key: String) -> Result<String, &'static str> {
+pub fn search(haystack: &str, search_key: &str) -> Result<String, &'static str> {
     if haystack.is_empty() || search_key.is_empty() {
         return Err("Invalid input - no object found");
     }
 
-    let search_path = parse_search_key(search_key);
-    // next need to iteratively parse haystack along search_path without parsing all of it
-    let mut curr_char = String::new();
-    let mut haystack_chars = haystack.chars();
-    let haystack_size = haystack.len();
+    let search_path = parse_search_key(search_key.to_string());
+    let json: Value = serde_json::from_str(haystack).expect("Invalid JSON");
+
+    // collect
+    let mut path_val = &json;
     for (path_pos, key) in search_path.iter().enumerate() {
-        if key.starts_with(JSON_OPEN_BRACKET) {
-            curr_char = haystack_chars.next().unwrap().to_string();
-            if curr_char != JSON_OPEN_BRACKET {
-                cancel_search_panic(&search_path, path_pos, haystack_chars.position().unwrap());
-            }
-            // TODO - arrays are broken by commas but each type needs to be understood in case there are
-            // dictionaries or nested lists
-            return Ok("NONE".to_string());
-        }
-        else {
-            curr_char = haystack_chars.nth(0).unwrap().to_string();
-            if curr_char != JSON_OPEN_BRACE {
-                cancel_search_panic(&search_path, path_pos, idx);
-            }
-            // e.g. "a.b.c" searched on {"b": {"a":"d"},"a":{"b":{"c":"e"}}} - going to first "a" key won't work
-            // Can slide to first "a" that is at the same depth by skipping over {} or []
-            let mut depth = path_pos;
-            let mut open_key = false;
-            let mut key_found = false;
-            let mut quoted_key = String::new();
-            quoted_key.push_str(JSON_QUOTE);
-            quoted_key.push_str(key);
-            quoted_key.push_str(JSON_QUOTE);
-            while haystack_chars.next < haystack_size {
-                if curr_char == JSON_OPEN_BRACE || curr_char == JSON_OPEN_BRACKET {
-                    depth += 1;
-                    idx += 1;
-                    continue;
-                }
-                else if curr_char == JSON_CLOSE_BRACE || curr_char == JSON_CLOSE_BRACKET {
-                    depth -= 1;
-                    if depth < path_pos {
-                        cancel_search_panic(&search_path, path_pos, idx);
-                    }
-
-                    idx += 1;
-                    continue;
-                }
-                else if curr_char == JSON_COLON || curr_char == JSON_COMMA {
-                    idx += 1;
-                    continue;
-                }
-
-                if depth > path_pos {
-                    idx += 1;
-                    continue;
-                }
-
-                if curr_char == JSON_QUOTE {
-                    if open_key {
-                        open_key = false;
-                        idx += 1;
-                        continue;
-                    }
-                    else {
-                        open_key = true;
-                        if quoted_key == &haystack[idx..key.len() + 1] {
-                            // found the key - break!
-                            key_found = true;
-                            println!("key found: {}[{}]", key, idx);
-                            return Ok(haystack[idx..idx + key.len() + 1].to_string());
-                        }
-                    }
-                }
-
-            }
-            if (!key_found) {
-                cancel_search_panic(&search_path, path_pos, depth);
-            }
-
+        let idx = array_ind(key.to_string());
+        if idx > -1 {
+            path_val = &path_val[idx.unsigned_abs() as usize];
+        } else {
+            path_val = &path_val[key];
         }
     }
 
-    Ok("done".to_string())
+    // If value is a string, need to treat it as_str to avoid adding surrounding '"'
+    if path_val.is_string() {
+        return Ok(path_val.as_str().unwrap().to_string());
+    }
+    Ok(path_val.to_string())
 }
 
 fn cancel_search_panic(search_path: &Vec<String>, path_pos_reached: usize, idx: usize) {
@@ -120,7 +50,7 @@ fn parse_search_key(search_key: String) -> Vec<String> {
 
 fn array_ind(accessor: String) -> i64 {
     if !ARRAY_REGEX.is_match(accessor.as_str()) {
-        return -1
+        return -1;
     }
 
     let val: i64 = match accessor.split(&['[', ']'][..]).nth(1) {
@@ -136,23 +66,42 @@ mod tests {
 
     #[test]
     fn empty_data() {
-        assert!(search("".to_string(), "a".to_string()).is_err());
+        assert!(search("", "a").is_err());
     }
 
     #[test]
     fn empty_search_key() {
-        assert!(search(r#"{"a": "b"}"#.to_string(), "".to_string()).is_err());
+        assert!(search(r#"{"a": "b"}"#, "").is_err());
     }
 
     #[test]
-    #[should_panic]
-    fn mismatched_search_key_no_array() {
-        let _ = search(r#"{"a": "b"}"#.to_string(), "[0].a".to_string());
+    fn object_search() {
+        assert_eq!(search(r#"{"b": {"a":"d"},"a":{"b":{"c":"e"}}}"#, "a.b"), Ok(r#"{"c":"e"}"#.to_string()));
+        assert_eq!(search(r#"
+            [
+                [1, [2, 3], "n", {"o": "p"}],
+                "c",
+                {
+                    "a": {
+                        "x": "y\"b\"",
+                        "b": [
+                            2,
+                            3,
+                            4
+                        ]
+                    }
+                },
+                [
+                    "d",
+                    "e"
+                ]
+            ]"#, "[2].a.b[1]"), Ok("3".to_string()));
     }
 
     #[test]
-    fn object_key_found() {
-        search(r#"{"b": {"a":"d"},"a":{"b":{"c":"e"}}}"#.to_string(), "a.b".to_string());
+    fn array_search() {
+        assert_eq!(search(r#"[{"x": "y"}, {"p":"q"}]"#, "[1].p"), Ok(r#"q"#.to_string()));
+        assert_eq!(search(r#"[{"x": "y"}, {"p":"\"q\""}]"#, "[1].p"), Ok(r#""q""#.to_string()));
     }
 
     #[test]
