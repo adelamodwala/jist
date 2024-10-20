3f28ddf60c3a179d6c77105711aa1aa6e29e51c0
adel <adel.amodwala@gmail.com>
2024-10-20 07:10:03 -0400

    Fix parsing search path with nested arrays; Implement object search; Add tests for general use cases;

--- src/parse_top_level.rs
+++ src/parse_top_level.rs
@@ -1,6 +1,6 @@
-use std::cmp::{Ordering};
 use crate::utils::array_ind;
 use json_tools::{Buffer, BufferType, Lexer, TokenType};
+use std::cmp::Ordering;
 
 fn token_pos(buf: &Buffer) -> Result<(u64, u64), &'static str> {
     let (first, end) = match buf {
@@ -8,13 +8,6 @@ fn token_pos(buf: &Buffer) -> Result<(u64, u64), &'static str> {
         _ => { return Err("error"); }
     };
     Ok((first, end))
-}
-
-fn is_primitive_token(token_type: TokenType) -> bool {
-    match token_type {
-        TokenType::String | TokenType::Number | TokenType::BooleanFalse | TokenType::BooleanTrue | TokenType::Null => true,
-        _ => false
-    }
 }
 
 fn checkpoint_depth(search_path: &Vec<String>, idx: usize) -> (i32, i32, i32) {
@@ -58,7 +51,7 @@ pub fn search(haystack: &str, search_path: &Vec<String>) -> Result<String, &'sta
         .map(|x| x.clone())
         .rev()
         .collect::<Vec<String>>();
-    println!("checkpoints: {:?}, arr_tgt: {:?}", checkpoints, arr_tgt);
+    println!("checkpoints: {:?}, arr_tgt: {:?}, search_keys: {:?}", checkpoints, arr_tgt, search_keys);
 
     loop {
         let mut token = token_iter.next().unwrap();
@@ -95,7 +88,7 @@ pub fn search(haystack: &str, search_path: &Vec<String>) -> Result<String, &'sta
             }
             _ => {}
         }
-        println!("depth_curr: {:?}, arr_idx: {:?}, kind: {:?}, last_open: {:?}, checkpoints: {:?}", depth_curr, arr_idx, &token.kind, last_open, checkpoints);
+        println!("depth_curr: {:?}, arr_idx: {:?}, search_keys: {:?}, kind: {:?}, last_open: {:?}, checkpoints: {:?}, checkpoint_start: {:?}", depth_curr, arr_idx, search_keys, &token.kind, last_open, checkpoints, checkpoint_start);
 
         if depth_curr.cmp(&checkpoints.last().unwrap()) == Ordering::Equal {
             // check if inside an array and iterating to find the expected place
@@ -104,12 +97,12 @@ pub fn search(haystack: &str, search_path: &Vec<String>) -> Result<String, &'sta
 
                 // terminal point
                 if checkpoints.len() == 1 && checkpoint_start.len() == arr_tgt_size {
-                    println!("***checkpoint ended***");
+                    println!("[checkpoint ended]");
 
                     // add 1 to starting index to exclude commas or brackets
-                    return Ok(haystack[checkpoint_start.last().unwrap() + 1..(end as usize)].trim().to_string());
+                    return Ok(haystack[checkpoint_start.last().unwrap() + 1..(end as usize)].trim().trim_start_matches("\"").trim_end_matches("\"").to_string());
                 } else {
-                    println!("***checkpoint started***");
+                    println!("[checkpoint started]");
 
                     checkpoint_start.push(first as usize);
                     if arr_tgt.len() > 1 {
@@ -120,6 +113,37 @@ pub fn search(haystack: &str, search_path: &Vec<String>) -> Result<String, &'sta
                 if checkpoints.len() > 1 {
                     checkpoints.pop();
                 }
+            } else if *last_open.last().unwrap() == TokenType::CurlyOpen {
+                if (token.kind == TokenType::CurlyOpen || token.kind == TokenType::Comma) && search_keys.len() > 0 {
+                    if token.kind == TokenType::CurlyOpen { println!("\\checkpoint started\\") }
+
+                    // next token should be a key
+                    token = token_iter.next().expect("incomplete json object");
+                    let (first, end) = token_pos(&token.buf)?;
+                    let key = haystack[first as usize..end as usize].trim_start_matches("\"").trim_end_matches("\"");
+                    if key.cmp(&search_keys.last().unwrap()) == Ordering::Equal {
+                        println!(">>>found next key: {:?}", key);
+                        search_keys.pop();
+                    }
+
+                    if checkpoints.len() > 1 {
+                        checkpoints.pop();
+                    }
+                } else if search_keys.len() == 0 && checkpoints.len() == 1 {
+                    let (first, end) = token_pos(&token.buf)?;
+                    match token.kind {
+                        TokenType::String | TokenType::Number | TokenType::BooleanFalse | TokenType::BooleanTrue | TokenType::Null => {
+                            return Ok(haystack[first as usize..(end as usize)].trim().trim_start_matches("\"").trim_end_matches("\"").to_string());
+                        }
+                        TokenType::Colon => {
+                            checkpoint_start.push(first as usize);
+                        }
+                        TokenType::CurlyClose | TokenType::BracketClose => {
+                            return Ok(haystack[checkpoint_start.last().unwrap() + 1..(end as usize)].trim().to_string());
+                        }
+                        _ => {}
+                    }
+                }
             }
         }
 
@@ -129,36 +153,30 @@ pub fn search(haystack: &str, search_path: &Vec<String>) -> Result<String, &'sta
     }
 }
 
-pub fn parse_test(data: &str) {
-    let token_iter = Lexer::new(data.bytes(), BufferType::Span);
-    for token in token_iter {
-        println!("{:?}", token);
-    }
-}
-
 #[cfg(test)]
 mod tests {
-    use crate::utils::parse_search_key;
     use super::*;
+    use crate::utils::parse_search_key;
+    use serde_json::Value;
 
     #[test]
     fn checkpoint_depth_test() {
-        assert_eq!(checkpoint_depth(&vec!["a".to_string(), "b".to_string(), "[1]".to_string()], 0), (0, -1, 0));
-        assert_eq!(checkpoint_depth(&vec!["a".to_string(), "b".to_string(), "[1]".to_string()], 1), (1, -1, 1));
-        assert_eq!(checkpoint_depth(&vec!["a".to_string(), "b".to_string(), "[1]".to_string()], 2), (2, 0, 1));
+        assert_eq!(checkpoint_depth(&parse_search_key("a.b[1]".to_string()), 0), (0, -1, 0));
+        assert_eq!(checkpoint_depth(&parse_search_key("a.b[1]".to_string()), 1), (1, -1, 1));
+        assert_eq!(checkpoint_depth(&parse_search_key("a.b[1]".to_string()), 2), (2, 0, 1));
 
-        assert_eq!(checkpoint_depth(&vec!["[2]".to_string()], 0), (0, 0, -1));
+        assert_eq!(checkpoint_depth(&parse_search_key("[2]".to_string()), 0), (0, 0, -1));
 
         assert_eq!(checkpoint_depth(&vec!["a".to_string()], 0), (0, -1, 0));
 
-        assert_eq!(checkpoint_depth(&vec!["[1]".to_string(), "[1]".to_string(), "[1]".to_string(), "b".to_string()], 0), (0, 0, -1));
-        assert_eq!(checkpoint_depth(&vec!["[1]".to_string(), "[1]".to_string(), "[1]".to_string(), "b".to_string()], 1), (1, 1, -1));
-        assert_eq!(checkpoint_depth(&vec!["[1]".to_string(), "[1]".to_string(), "[1]".to_string(), "b".to_string()], 2), (2, 2, -1));
-        assert_eq!(checkpoint_depth(&vec!["[1]".to_string(), "[1]".to_string(), "[1]".to_string(), "b".to_string()], 3), (3, 2, 0));
+        assert_eq!(checkpoint_depth(&parse_search_key("[1][1][1].b".to_string()), 0), (0, 0, -1));
+        assert_eq!(checkpoint_depth(&parse_search_key("[1][1][1].b".to_string()), 1), (1, 1, -1));
+        assert_eq!(checkpoint_depth(&parse_search_key("[1][1][1].b".to_string()), 2), (2, 2, -1));
+        assert_eq!(checkpoint_depth(&parse_search_key("[1][1][1].b".to_string()), 3), (3, 2, 0));
     }
 
     #[test]
-    fn simple_array() {
+    fn array_only() {
         assert_eq!(search(r#"[8,9,1]"#, &vec!("[0]".to_string())), Ok("8".to_string()));
         assert_eq!(search(r#"[8,9,1]"#, &vec!("[1]".to_string())), Ok("9".to_string()));
         assert_eq!(search(r#"[8,9,1]"#, &vec!("[2]".to_string())), Ok("1".to_string()));
@@ -166,53 +184,71 @@ mod tests {
         assert_eq!(search(r#"[{"x":"y"},{"a":{"b": "c"}},1]"#, &vec!("[0]".to_string())), Ok(r#"{"x":"y"}"#.to_string()));
         assert_eq!(search(r#"[{"x":"y"},{"a":{"b": "c"}},1]"#, &vec!("[1]".to_string())), Ok(r#"{"a":{"b": "c"}}"#.to_string()));
 
-        assert_eq!(search(r#"[[3, [6,7],5],9,1]"#, &vec!("[0]".to_string(), "[1]".to_string(), "[1]".to_string())), Ok("7".to_string()));
+        assert_eq!(search(r#"[[3, [6,7],5],9,1]"#, &parse_search_key("[0][1][1]".to_string())), Ok("7".to_string()));
         assert_eq!(search(r#"[[3, [6,7],5],9,1]"#, &vec!("[0]".to_string(), "[1]".to_string())), Ok("[6,7]".to_string()));
         assert_eq!(search(r#"[[3, [6,7],5],9,1]"#, &vec!("[2]".to_string())), Ok("1".to_string()));
         assert_eq!(search(r#"[[3, [6,7],5],9,1]"#, &vec!("[0]".to_string())), Ok("[3, [6,7],5]".to_string()));
-        // assert_eq!(search(r#"
-        //     [
-        //         [-140.5405, [2, 3], "n", {"o": "p"}],
-        //         "c",
-        //         {
-        //             "a": {
-        //                 "x": "y\"b\"",
-        //                 "b": [
-        //                     2,
-        //                     3,
-        //                     4
-        //                 ]
-        //             }
-        //         },
-        //         {
-        //             "a": {
-        //                 "x": "y\"b\"",
-        //                 "b": [
-        //                     2,
-        //                     7,
-        //                     4
-        //                 ]
-        //             }
-        //         },
-        //         [
-        //             "d",
-        //             "e"
-        //         ]
-        //     ]"#, &parse_search_key("[3].a.b".to_string())), Ok("7".to_string()));
-        // assert_eq!(search(r#"
-        //     {
-        //         "a": {
-        //             "x": "y\"b\"",
-        //             "b": [
-        //                 2,
-        //                 3,
-        //                 4
-        //             ]
-        //         }
-        //     }"#, &parse_search_key("a.b".to_string())), Ok("7".to_string()));
     }
 
-    fn simple_object() {
+    #[test]
+    fn object_only() {
+        assert_eq!(search(r#"{"x":"y"}"#, &vec!("x".to_string())), Ok("y".to_string()));
 
+        assert_eq!(search(r#"{"x":{"y":"z"}}"#, &parse_search_key("x.y".to_string())), Ok("z".to_string()));
+        assert_eq!(search(r#"{"x":{"y":"z"}}"#, &vec!("x".to_string())), Ok(r#"{"y":"z"}"#.to_string()));
+
+        assert_eq!(search(r#"{"x":["y"]}"#, &vec!("x".to_string())), Ok(r#"["y"]"#.to_string()));
+        assert_eq!(search(r#"{"x":{"y":["z"]}}"#, &vec!("x".to_string())), Ok(r#"{"y":["z"]}"#.to_string()));
+        assert_eq!(search(r#"{"x":{"y":["z"]}}"#, &parse_search_key("x.y".to_string())), Ok(r#"["z"]"#.to_string()));
+        assert_eq!(search(r#"{"x":{"y":["z"]}}"#, &parse_search_key("x.y[0]".to_string())), Ok("z".to_string()));
+    }
+
+    #[test]
+    fn mixed() {
+        let mut result = search(r#"
+            [
+                [-140.5405, [2, 3], "n", {"o": "p"}],
+                "c",
+                {
+                    "a": {
+                        "x": "y\"b\"",
+                        "b": [
+                            2,
+                            3,
+                            4
+                        ]
+                    }
+                },
+                {
+                    "a": {
+                        "x": "y\"b\"",
+                        "b": [
+                            2,
+                            7,
+                            4
+                        ]
+                    }
+                },
+                [
+                    "d",
+                    "e"
+                ]
+            ]"#, &parse_search_key("[3].a.b".to_string())).unwrap();
+        let mut json: Value = serde_json::from_str(&result).expect("Invalid JSON");
+        assert_eq!(json.to_string(), "[2,7,4]".to_string());
+
+        result = search(r#"
+            {
+                "a": {
+                    "x": "y\"b\"",
+                    "b": [
+                        2,
+                        3,
+                        4
+                    ]
+                }
+            }"#, &parse_search_key("a.b".to_string())).unwrap();
+        json = serde_json::from_str(&result).expect("Invalid JSON");
+        assert_eq!(json.to_string(), "[2,3,4]".to_string());
     }
 }
--- src/utils.rs
+++ src/utils.rs
@@ -3,6 +3,7 @@ use regex::Regex;
 
 lazy_static! {
     static ref ARRAY_REGEX: Regex = Regex::new(r"^\[(\d+)\]$").unwrap();
+    static ref SPLIT_JSON_PATH_REGEX: Regex = Regex::new(r"\[(?:[^\[\]]*)\]|[^.\[\]]+").unwrap();
 }
 
 pub(crate) fn array_ind(accessor: String) -> i64 {
@@ -18,13 +19,9 @@ pub(crate) fn array_ind(accessor: String) -> i64 {
 }
 
 pub(crate) fn parse_search_key(search_key: String) -> Vec<String> {
-    search_key.split(&['.'][..]).map(|s| {
-        let s = match s.find("[") {
-            Some(i) => s.split_at(i),
-            None => (s, ""),
-        };
-        vec![s.0, s.1]
-    }).flatten().filter(|s| !s.is_empty()).map(|s| s.to_string()).collect()
+    SPLIT_JSON_PATH_REGEX.find_iter(search_key.as_str())
+        .map(|m| m.as_str().to_string())
+        .collect()
 }
 
 #[cfg(test)]
@@ -45,5 +42,8 @@ mod tests {
         assert_eq!(parse_search_key("myroot.child1[0]".to_string()), vec!["myroot", "child1", "[0]"]);
         assert_eq!(parse_search_key("myroot.child1[0].arr1".to_string()), vec!["myroot", "child1", "[0]", "arr1"]);
         assert_eq!(parse_search_key("[2].child1[0].arr1".to_string()), vec!["[2]", "child1", "[0]", "arr1"]);
+        assert_eq!(parse_search_key("[1][1][1].b".to_string()), vec!["[1]", "[1]", "[1]", "b"]);
+        assert_eq!(parse_search_key("x.y[1][1][1].b".to_string()), vec!["x", "y","[1]", "[1]", "[1]", "b"]);
+        assert_eq!(parse_search_key("x.y[1][1][1].b[1222][439834]".to_string()), vec!["x", "y","[1]", "[1]", "[1]", "b", "[1222]", "[439834]"]);
     }
 }
